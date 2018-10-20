da7d75da0ad7159113c02d258f26912ed0dba5bd
Handy Hasan <hhas0002@student.monash.edu>
2018-10-20 20:04:19 +1100

    second commit

--- dist/test.js
+++ dist/test.js
@@ -6,25 +6,39 @@ function paste(){
             a = clipText
             
             document.getElementById("currentText").innerText = clipText 
-            
+            $('#currentText').find('br').replaceWith( " " );
         } 
     );  
 }
 
+function createMarquee(){
+
+}
+
 function startMarquee(){
+    if ($(".border").find("#anotherdiv").length > 0){ 
+        $( "#anotherdiv" ).remove();
+      }
     var text = document.createElement("marquee")
     text.setAttribute("id","anotherdiv")
     text.setAttribute("scrollamount","15")
     text.innerText = a
-    
     document.getElementById("currentText").innerHTML = ""
-    document.getElementById("currentText").appendChild(text);
+    document.getElementById("insideborder").appendChild(text);
     $( "br" ).replaceWith( " " );
+    $('br').remove()
 }
 
-function changeSpeed(){
+function reset(){
+    console.log("test")
+    setTimeout(function() {
 
+        startMarquee()
+      }, 1000);
 }
+
+
+
 
 function fontSize(){
 
